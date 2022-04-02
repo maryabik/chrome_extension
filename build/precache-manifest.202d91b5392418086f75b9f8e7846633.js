fd298295bb106fd82912b1968c52c669b974268d
temilola <malkyli.abik@gmail.com>
2022-04-02 14:31:06 -0700

    Removed node_modules

---
+++ build/precache-manifest.202d91b5392418086f75b9f8e7846633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "961812826fff6ec046e8",
+    "url": "/static/css/main.cafa7862.chunk.css"
+  },
+  {
+    "revision": "961812826fff6ec046e8",
+    "url": "/static/js/main.96181282.chunk.js"
+  },
+  {
+    "revision": "8fff69a20f34251f7898",
+    "url": "/static/js/1.8fff69a2.chunk.js"
+  },
+  {
+    "revision": "e3221f681961d784334d",
+    "url": "/static/js/2.e3221f68.chunk.js"
+  },
+  {
+    "revision": "3c2378a1d2777c9452c6",
+    "url": "/static/js/runtime~main.3c2378a1.js"
+  },
+  {
+    "revision": "dbbba03ab59ddc2d542b47ca5c19187c",
+    "url": "/index.html"
+  }
+];
